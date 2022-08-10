58884ce3504fd3895788305e267a680a94ca156c
jh3p <110979205+jh3p@users.noreply.github.com>
2022-08-10 19:15:50 +0800

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,74 @@
+let allMusic = [
+  {
+    name: "Ours",
+    artist: "Taylor Swift",
+    img: "img-2",
+    src: "Ours - Taylor Swift"
+  },
+  {
+    name: "Mine",
+    artist: "Taylor Swift",
+    img: "img-1",
+    src: "Mine - Taylor Swift"
+  },
+   {
+    name: "I'd Lie",
+    artist: "Taylor Swift",
+    img: "img-3",
+    src: "I'd Lie - Taylor Swift"
+  },
+   {
+    name: "An Art Gallery Could Never Be As Unique As You",
+    artist: "mrld",
+    img: "img-4",
+    src: "An Art Gallery Could Never Be As Unique As You - mrld"
+  },
+  {
+    name: "Lows",
+    artist: "Pink Sweats",
+    img: "img-5",
+    src: "Lows - Pink Sweats"
+  },
+  {
+    name: "Grow Old with You",
+    artist: "Adam Sandler",
+    img: "img-6",
+    src: "Grow Old With You - Adam Sandler"
+  },
+  {
+    name: "Best Part of Me",
+    artist: "Ed Sheeran ft. YEBBA",
+    img: "img-7",
+    src: "Best Part of Me - Ed Sheeran ft. YEBBA"
+  },
+  {
+    name: "Real Thing",
+    artist: "Pink Sweats",
+    img: "img-8",
+    src: "Real Thing - Pink Sweats"
+  },
+  {
+    name: "Sol at Luna",
+    artist: "Geiko",
+    img: "img-9",
+    src: "Sol at Luna - Geiko"
+  },
+  {
+    name: "Lifetime",
+    artist: "Justin Bieber",
+    img: "img-10",
+    src: "Lifetime - Justin Bieber"
+  },
+  {
+    name: "Midnight River",
+    artist: "Pink Sweats ft. Tory Kelly",
+    img: "img-11",
+    src: "Midnight River - Pink Sweats"
+  },
+  {
+    name: "I Don't Care",
+    artist: "Jeremy Passion",
+    img: "img-12",
+    src: "I Don't Care - Jeremy Passion"
+  },
+];
